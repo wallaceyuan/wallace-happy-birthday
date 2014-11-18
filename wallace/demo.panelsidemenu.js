af0b33421fdfeca096f3ce08d48ba467a8fa1fb9
wallaceyuan <1272864289@qq.com>
2014-11-18 17:34:33 +0800

    111734

--- wallace/demo.panelsidemenu.js
+++ wallace/demo.panelsidemenu.js
@@ -9,11 +9,6 @@ define({
         <li><a href="#" data-src="http://wallaceyuan.github.io/audiojs/demos/mp3/天涯明月刀.mp3">天涯明月刀</a></li>\
         <li><a href="#" data-src="http://wallaceyuan.github.io/audiojs/demos/mp3/有一天我们都会老.mp3">有一天我们都会老</a></li>\
         <li><a href="#" data-src="http://wallaceyuan.github.io/audiojs/demos/mp3/中国山.mp3">中国山</a></li>\
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/07-everyday-shelter.mp3">everyday shelter</a></li>\
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/08-basic-hypnosis.mp3">basic hypnosis</a></li>\
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/09-infinite-victory.mp3">infinite victory</a></li>\
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/10-the-curious-incident-of-big-poppa-in-the-nighttime.mp3">the curious incident of big poppa in the nighttime</a></li>\
-        <li><a href="#" data-src="http://kolber.github.io/audiojs/demos/mp3/11-mo-stars-mo-problems.mp3">mo stars mo problems</a></li>\
       </ul>\
       <audio preload></audio>\
     </div>\
