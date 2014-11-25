2b4383228dba7dbf29f202d3f31d6c89554f09fd
wallaceyuan <1272864289@qq.com>
2014-11-25 18:16:10 +0800

    201411251816

--- wallace/demo.imgone.js
+++ wallace/demo.imgone.js
@@ -1,6 +1,6 @@
 define({
 	title: '图片',
-	body:'<div class="imgfor"><div class="pageone"></div><div class="swiper-container" id="sliderone"><div class="swiper-wrapper"><div class="swiper-slide"><img src="images/loading.gif" data-src="images/9.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/10.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/11.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/12.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/14.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/15.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/16.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/17.jpg"></div></div></div></div>\
+	body:'<div class="imgfor"><div class="page" id="sw"><img src="images/swipe.png" width="45"></div><div class="swiper-container" id="sliderone"><div class="swiper-wrapper"><div class="swiper-slide"><img src="images/loading.gif" data-src="images/9.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/10.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/11.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/12.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/14.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/15.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/16.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/17.jpg"></div></div></div></div>\
 		',
 	init: function(pageData) {
 		var $view = this
@@ -12,20 +12,14 @@ define({
 		}
 		
 		$('pre', $view).each(function(i, e) { hljs.highlightBlock(e) })
-		
-		if(swiperNested){
-			swiperNested.destroy();
-		}
-		
+
+		var $window = $(window);
+		var winHeight =$window.height();
+
+		$(".swiper-container,.swiper").css("height",winHeight);
 		var swiperNested = new Swiper('#sliderone',{
 
-			pagination: '.pageone',
-
 			mode:"horizontal",
-			
-			paginationClickable: true,
-
-			mousewheelControl:true
 
 		})
 
--- wallace/demo.imgtwo.js
+++ wallace/demo.imgtwo.js
@@ -1,6 +1,6 @@
 define({
 	title: '图片',
-	body:'<div class="imgfor"><div class="pagetwo"></div><div class="swiper-container" id="slidertwo"><div class="swiper-wrapper"><div class="swiper-slide"><img src="images/loading.gif" data-src="images/18.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/19.jpg"></div>s<div class="swiper-slide"><img src="images/loading.gif" data-src="images/20.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/21.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/22.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/23.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/24.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/25.jpg"></div></div></div></div>\
+	body:'<div class="imgfor"><div class="page" id="sw"><img src="images/swipe.png" width="45"></div><div class="swiper-container" id="slidertwo"><div class="swiper-wrapper"><div class="swiper-slide"><img src="images/loading.gif" data-src="images/18.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/19.jpg"></div>s<div class="swiper-slide"><img src="images/loading.gif" data-src="images/20.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/21.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/22.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/23.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/24.jpg"></div><div class="swiper-slide"><img src="images/loading.gif" data-src="images/25.jpg"></div></div></div></div>\
 		',
 	init: function(pageData) {
 		var $view = this
@@ -22,13 +22,7 @@ define({
 		}
 		var swiperNested = new Swiper('#slidertwo',{
 
-			pagination: '.pagetwo',
-
 			mode:"horizontal",
-
-			paginationClickable: true,
-
-			mousewheelControl:true
 
 		})
 
